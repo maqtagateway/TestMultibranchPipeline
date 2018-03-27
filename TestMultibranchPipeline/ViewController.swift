bd6fcf7bec89b8014f6c7679b292977fc46a071d
Ahsan Iqbal <muhammadahsan.iqbal@adports.ae>
2018-03-27 10:25:19 +0400

    Initial commit

---
+++ TestMultibranchPipeline/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestMultibranchPipeline
+//
+//  Created by Ahsan Iqbal on 3/27/18.
+//  Copyright © 2018 Maqta Gateway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
